b0705ba3f4e934b0d3cae5ed6a853be1477239ff
Guido Schmidt <git@guidoschmidt.cc>
2021-03-21 23:28:53 +0100

    FIX/src/buttons: mobile positioning for narrative buttons, EDIT/kontrol

--- src/kontrol/Kontrol.js
+++ src/kontrol/Kontrol.js
@@ -11,48 +11,52 @@ const Kontrol = ({ config }) => {
         <span className="emoji">⚙️</span>
       </button>
       <div className={["kontrol", open ? "open" : "closed"].join(" ")}>
-        {Object.keys(config).map((key) => {
-          const p = config[key];
-          switch (p.type) {
-            case "range": {
-              return (
-                <div className="row" key={key}>
-                  <label>{p.label}</label>
-                </div>
-              );
-              break;
-            }
+        {Object.keys(config)
+          .filter((key) =>
+            config[key].hasOwnProperty("hidden") ? !config[key].hidden : true
+          )
+          .map((key) => {
+            const p = config[key];
+            switch (p.type) {
+              case "range": {
+                return (
+                  <div className="row" key={key}>
+                    <label>{p.label}</label>
+                  </div>
+                );
+                break;
+              }
 
-            case "button": {
-              return (
-                <button className="kontrol-btn" onClick={p.action}>
-                  <span className="emoji">{p.icon}</span>
-                  {p.label}
-                </button>
-              );
-              break;
-            }
-            case "file": {
-              return (
-                <div className="kontrol-fileupload">
-                  <label htmlFor="upload" className="btn btn-file">
+              case "button": {
+                return (
+                  <button className="kontrol-btn" onClick={p.action}>
                     <span className="emoji">{p.icon}</span>
                     {p.label}
-                  </label>
-                  <input
-                    id="upload"
-                    type="file"
-                    onChange={p.action}
-                    style={{ display: "none" }}
-                  />
-                </div>
-              );
+                  </button>
+                );
+                break;
+              }
+              case "file": {
+                return (
+                  <div className="kontrol-fileupload">
+                    <label htmlFor="upload" className="btn btn-file">
+                      <span className="emoji">{p.icon}</span>
+                      {p.label}
+                    </label>
+                    <input
+                      id="upload"
+                      type="file"
+                      onChange={p.action}
+                      style={{ display: "none" }}
+                    />
+                  </div>
+                );
+              }
+              default:
+                return <></>;
+                break;
             }
-            default:
-              return <></>;
-              break;
-          }
-        })}
+          })}
       </div>
     </>
   );
