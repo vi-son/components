dd7f8a8a8c84589e01ab80744bfcda4111879b77
Guido Schmidt <git@guidoschmidt.cc>
2021-03-28 01:51:08 +0100

    EDIT/kontrol/Kontrol: improve kontrol ui

--- src/kontrol/Kontrol.js
+++ src/kontrol/Kontrol.js
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 // Style imports
 import "./Kontrol.sass";
 
-const Kontrol = ({ config }) => {
+const Kontrol = ({ config, children }) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <>
+    <div>
       <button className="kontrol-toggle" onClick={() => setOpen(!open)}>
         <span className="emoji">⚙️</span>
       </button>
@@ -15,12 +15,12 @@ const Kontrol = ({ config }) => {
           .filter((key) =>
             config[key].hasOwnProperty("hidden") ? !config[key].hidden : true
           )
-          .map((key) => {
+          .map((key, i) => {
             const p = config[key];
             switch (p.type) {
               case "range": {
                 return (
-                  <div className="row" key={key}>
+                  <div className="row" key={`${key}.${i}`}>
                     <label>{p.label}</label>
                   </div>
                 );
@@ -29,16 +29,21 @@ const Kontrol = ({ config }) => {
 
               case "button": {
                 return (
-                  <button className="kontrol-btn" onClick={p.action}>
+                  <button
+                    className="kontrol-btn"
+                    onClick={p.action}
+                    key={`${key}.${i}`}
+                  >
                     <span className="emoji">{p.icon}</span>
                     {p.label}
                   </button>
                 );
                 break;
               }
+
               case "file": {
                 return (
-                  <div className="kontrol-fileupload">
+                  <div className="kontrol-fileupload" key={`${key}.${i}`}>
                     <label htmlFor="upload" className="btn btn-file">
                       <span className="emoji">{p.icon}</span>
                       {p.label}
@@ -52,13 +57,20 @@ const Kontrol = ({ config }) => {
                   </div>
                 );
               }
+
+              case "divider":
+                return (
+                  <div className="kontrol-divider" key={`${key}.${i}`}></div>
+                );
+
               default:
                 return <></>;
                 break;
             }
           })}
+        {children}
       </div>
-    </>
+    </div>
   );
 };
 
